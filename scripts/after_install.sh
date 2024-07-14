a092a50f900cfb0b5af8f911be692b19d9b9d727
Jbabajohn <john.toriola.tj@gmail.com>
2024-07-14 12:42:33 +0100

    troubleshooting

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+echo "Checking location before switching"
+ls -al
+pwd
 # cd /home/ec2-user/server_backend
 sudo chown -R ec2-user:ec2-user /home/ec2-user/server_backend
+echo "Checking location after switching"
+ls -al
+pwd
 
 sudo yum install nodejs -y
 sudo npm install -g npm@10.8.2
